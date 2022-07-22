e7d0d7a10be1d151c3a016682b7537dffebb7f54
Rochmad <rochmad.wa@gmail.com>
2022-07-22 08:38:05 +0700

    fix: ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  logging: process.env.NODE_ENV === 'development',
+  entities: ['dist/src/**/entities/*.js'],
+  migrationsTableName: 'migration_table',
+  migrations: ['dist/database/migrations/*.js'],
+  seeds: ['dist/database/seeds/*.js'],
+  cli: {
+    migrationsDir: 'database/migrations',
+  },
+};
