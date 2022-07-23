9a010341377b0ef56b110409399ac809877637d4
Rochmad <rochmad.wa@gmail.com>
2022-07-23 08:22:16 +0700

    fix: migration

---
+++ database/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { cwd, env } from 'process';
+
+dotenv.config();
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: env.DB_HOST,
+  port: parseInt(env.DB_PORT),
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  logging: env.NODE_ENV === 'development',
+  entities: ['dist/src/**/entities/*.js'],
+  migrationsTableName: 'migration_table',
+  migrations: ['dist/database/migrations/*.js'],
+});
