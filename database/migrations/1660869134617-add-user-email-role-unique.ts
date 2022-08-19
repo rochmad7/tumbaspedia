013e70f72643b025f456eda3d6489cc0dc69851f
Rochmad <rochmad.wa@gmail.com>
2022-08-19 08:08:12 +0700

    feat: add email and role unique constraint

---
+++ database/migrations/1660869134617-add-user-email-role-unique.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner, TableUnique } from 'typeorm';
+
+export class addUserEmailRoleUnique1660869134617 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3"`);
+
+    await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email", "role")`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3"`);
+
+    await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email")`);
+  }
+}
